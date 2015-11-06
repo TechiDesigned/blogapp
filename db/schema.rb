8960534732f57545a35ab0d5ca35881ce5299003
Ski Blanchard <ski.blanchard@careerbuilder.com>
2015-11-06 12:19:03 -0500

    Associated posts and comments + added post dropdown to comment form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105211258) do
+ActiveRecord::Schema.define(version: 20151106164120) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "message"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
