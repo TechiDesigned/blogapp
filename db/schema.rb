bd8403e870d0e061b4f5c376f14433538edf2dc3
Ski Blanchard <ski.blanchard@careerbuilder.com>
2015-11-13 11:12:25 -0500

    Created categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106164120) do
+ActiveRecord::Schema.define(version: 20151113160934) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "category_name"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "message"
